708d855ea5df5ee6fa9e8f5d9fed20e5ada6357f
stu <stthall@icloud.com>
2020-07-08 14:21:28 -0700

    building routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom'
+
 import HomePage from './pages/homepage/homepage.component'
 import './App.css';
+
+const HatsPage = ()=>{
+  return(
+    <div>
+      <h1>HATS PAGE</h1>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div>
-      <HomePage/>
+      <Switch>
+        <Route exact path='/' component={HomePage}/>
+        <Route exact path='/hats' component={HatsPage}/>
+      </Switch>
     </div>
   );
 }
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -10,28 +10,33 @@ class Directory extends React.Component{
         {
           title:'hats',
           imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-          id:1
+          id:1,
+          linkUrl:'hats',
         },
         {
           title:'jackets',
           imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-          id:2
+          id:2,
+          linkUrl:'',
         },
         {
           title:'sneakers',
           imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-          id:3
+          id:3,
+          linkUrl:'',
         },
         {
           title:'womens',
           imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
           id:4,
+          linkUrl:'',
           size:'large'
         },
         {
           title:'mens',
           imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
           id:5,
+          linkUrl:'',
           size:'large'
         },
       ]
@@ -40,8 +45,8 @@ class Directory extends React.Component{
   render(){
     return(
       <div className='directory-menu'>
-        {this.state.sections.map(({ id, title, imageUrl, size })=>(
-            <MenuItem key={id} title={title} imageUrl={imageUrl} size={size}/>
+        {this.state.sections.map(({ id, ...otherProps })=>(
+            <MenuItem key={id} {...otherProps}/>
         ))}
       </div>
     )
