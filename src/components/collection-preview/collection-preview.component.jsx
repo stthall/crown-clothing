6799a6c915d31835a57a83a5368010a941c23bce
stu <stthall@icloud.com>
2020-07-20 17:11:41 -0700

    +cart.utils, +grouping duplicate cart items, +updated shop_data

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -7,9 +7,9 @@ const CollectionPreview = ({ title, items })=>(
     <h1 className='title'>{ title.toUpperCase()}</h1>
     <div className='preview'>
       {items 
-        .filter((item,idx)=>idx < 4)
-        .map(({ id, ...otherProps })=>(
-          <CollectionItem key={id} {...otherProps}/>
+        .filter((item,index)=> index < 4)
+        .map( item =>(
+          <CollectionItem key={item.id} item={item}/>
         ))}
     </div>
   </div>
