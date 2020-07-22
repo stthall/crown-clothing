013f4b8f037503eebe8d4519ea871fe4ef7bc7fb
stu <stthall@icloud.com>
2020-07-22 09:26:27 -0700

    +addItem, + removeItem, completed checkout-item component

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -9,3 +9,17 @@ export const addItemToCart = (cartItems, newItem) =>{
   }
   return [...cartItems, {...newItem, quantity:1}]
 }
+
+export const removeItemFromCart = (cartItems, cartItemToRemove)=>{
+  const existingCartItems = cartItems.find(
+    cartItem => cartItem.id === cartItemToRemove.id
+  )
+  if(existingCartItems.quantity === 1){
+    return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id)
+  }
+  return cartItems.map(
+    cartItem => cartItem.id === cartItemToRemove.id
+      ? {...cartItem, quantity: cartItemToRemove.quantity - 1}
+      : cartItem
+  )
+}
