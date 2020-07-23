df04c5efdacd959b992d10711b7ac31d06e7b321
stu <stthall@icloud.com>
2020-07-23 12:21:49 -0700

    +directory.reducer, +shop.reducer, +directory.selectors, +ux flow for diretory and shop

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,24 +1,12 @@
 import React from 'react'
-import SHOP_DATA from './shop.data'
+import { Route } from 'react-router-dom'
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component'
+import CollectionPage from '../../pages/collection/collection.component'
 
-import CollectionPreview from '../../components/collection-preview/collection-preview.component'
-
-class ShopPage extends React.Component{
-  constructor(props){
-    super(props)
-    this.state={
-      collections: SHOP_DATA
-    }
-  }
-  render(){
-    const {collections} = this.state
-    return (<div className='shop-page'>
-        {
-          collections.map(({id, ...otherProps})=>(
-            <CollectionPreview key={id} {...otherProps}/>
-          ))
-        }
-      </div>)
-  }
-}
+const ShopPage = ({ match }) => (
+  <div className='shop-page'>
+    <Route exact path={`${match.path}`} component={CollectionsOverview} />
+    <Route exact path={`${match.path}/:collectionId`} component={CollectionPage}/>
+  </div>
+)
 export default ShopPage
